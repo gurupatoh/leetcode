d664c5161dadaf6448976885c951c48146634047
gurupatoh <patrickn0023@gmail.com>
2024-02-19 06:21:18 -0500

    leet code

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+
+def palindrome(word):
+    if len(word) <= 1:
+        return True
+    if word[0] != word[-1]:
+        return False
+    return palindrome(word[1:-1])
+
+word="soop"
+test_palindrome=palindrome(word)
+print("Test plindrome:",test_palindrome)
+
